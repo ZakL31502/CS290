f766fa76b412c26abdee0077649d7d681f1b05d7
Zak <zakarieleskowsky@gmail.com>
2022-11-07 23:36:25 -0800

    Final Changes Assignment3

--- Assignment3/index.js
+++ Assignment3/index.js
@@ -8,7 +8,10 @@
 
 
 //Make a backup copy of our posts for safety
-var postsBackup = document.getElementById("posts")
+var postsTracker = document.getElementById("posts")
+var postsReal = document.getElementsByClassName("post")
+var postsBackup = [...postsReal]
+
 
 //Function will toggle the hide on the modal and it's backdrop
 function handleSellButton(event){
@@ -79,7 +82,21 @@ function generatePost(newPrice, newCity, newCondition, newPhoto, newDescription)
     newLocation.classList.add("post-city")
     newLocation.textContent = "(" + newCity + ")"
     newInfoContainer.appendChild(newLocation)
-    console.log(newPost)
+
+    //Extra Credit: add new city to applicable lists
+    var listOfCities = document.getElementById("filter-city")
+    var cityMatch = 0
+    for(let i = 0; i < listOfCities.length; i++){
+      if(listOfCities[i].text === newCity){
+        cityMatch++
+      }
+    }
+    if(!cityMatch){ //No matching cities
+      var makeCity = document.createElement("option")
+      makeCity.text = newCity.charAt(0).toUpperCase() + newCity.slice(1)    //Line taken from https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+      listOfCities.appendChild(makeCity)
+    }
+
     return newPost
 }
 
@@ -116,7 +133,9 @@ function handleConfirmSale(event){
 
     
     var createdPost = generatePost(postPrice, postCity, postQuality, postPhoto, postDescription)
-    postsBackup.appendChild(createdPost)
+    postsTracker.appendChild(createdPost)
+    postsReal = document.getElementsByClassName("post")
+    postsBackup = [...postsReal];
     handleSellButton()
     
 }
@@ -138,42 +157,109 @@ Filter IDs: (overall class: "filter-input")
 
 
 //Function to handle price filtering
-function filterByPrice(shownPosts, priceAmount, minOrMax) {
-    for(let i = 0; i < shownPosts.length; i++){
-      console.log("shownPosts ", i, ": ", shownPosts[i])
-      if (minOrMax === 0){  //LOW = Min
-
+function filterByPrice(priceAmount, minOrMax) {
+    var eachPost = document.getElementsByClassName("post")
+    for(let i = eachPost.length-1; i >= 0; i--){
+      if (minOrMax === 0 && parseInt(eachPost[i].dataset.price) < priceAmount){  //LOW = Min
+        eachPost[i].remove()
       }
-      else{   //HIGH = Max
-
+      else if (minOrMax === 1 && parseInt(eachPost[i].dataset.price) > priceAmount){   //HIGH = Max
+        eachPost[i].remove()
       }
     }
 }
 
+//Function to handle text filtering
+function filterByText(enteredText) {
+  var eachPost = document.getElementsByClassName("post")
+  var eachText = document.getElementsByClassName("post-title")
+    for(let i = eachPost.length-1; i >= 0; i--){  //For each post, break down it's text
+      var postText = eachText[i].text.replace(/[!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~]/g, '').toLowerCase().split(' ');
+      console.log(postText)
+      var wordMatch = 0
+      for(let j = 0; j < postText.length; j++){
+        if (enteredText.toLowerCase() === postText[j]){  //If we find a match
+          wordMatch++
+        }
+      }
+      if(wordMatch === 0){  //There were no matches
+        eachPost[i].remove()
+      }
+    }
+}
+
+//Function to handle city filtering
+function filterByCity(checkedCity) {
+  if (checkedCity === "Any"){
+    return;   //Base case handling
+  }
+
+  var eachPost = document.getElementsByClassName("post")
+  for(let i = eachPost.length-1; i >= 0; i--){
+    if (checkedCity != eachPost[i].dataset.city){
+      eachPost[i].remove()
+    }
+  }
+}
+
+function filterByCondition() {
+  var eachPost = document.getElementsByClassName("post")
+  var conditionBoxes = document.querySelectorAll("input[name='filter-condition']:checked")
+
+  if (conditionBoxes.length === 0){
+    return  //When none checked, return
+  }
+  
+  for(let i = eachPost.length-1; i >= 0; i--){
+    var conditionMatch = 0
+    for (let j = 0; j < conditionBoxes.length; j++){
+      if (eachPost[i].dataset.condition === conditionBoxes[j].value){
+        conditionMatch++
+      }
+    }
+    if (!conditionMatch){ //If there was no match
+      eachPost[i].remove()
+    }
+  }
+
+}
+
+//Function to handle restoring 
+function restorePosts(backup) {
+  var eachPost = document.getElementsByClassName("post")
+  for(let i = eachPost.length-1; i >= 0; i--) {
+      eachPost[i].remove()
+  }
+  for(let i = 0; i < backup.length; i++) {
+    postsTracker.appendChild(backup[i])
+  }
+}
+
 //Function to handle when filters are applied
 function handleFilters(event) {
-    var shownPosts = postsBackup  //Create a new copy of the posts to preserve them
+    restorePosts(postsBackup)
 
     var filteredText = document.getElementById("filter-text").value
     var filteredMinPrice = document.getElementById("filter-min-price").value
     var filteredMaxPrice = document.getElementById("filter-max-price").value
-    var filteredCity = document.querySelector("input[name='filter-city']:checked")
-    console.log("Minprice: ", filteredMinPrice)
-    console.log("Maxprice: ", filteredMaxPrice)
+    var filteredCityOptions = document.getElementById("filter-city")
+    var filteredCity = filteredCityOptions.options[filteredCityOptions.selectedIndex].text
     //handle condition in a different way
 
     if(filteredMinPrice){
-        shownPosts = filterByPrice(shownPosts, filteredMinPrice, 0)
+      filterByPrice(filteredMinPrice, 0)
     }
     if(filteredMaxPrice){
-      shownPosts = filterByPrice(shownPosts, filteredMaxPrice, 1)
+      filterByPrice(filteredMaxPrice, 1)
     }
     if(filteredText){
-
+      filterByText(filteredText)
     }
-    if(filteredCity != "any"){
-
+    if(filteredCity != "Any"){
+      filterByCity(filteredCity)
     }
+
+    filterByCondition()
 }
 
 
